f62a846fc12155d9a994333a518bff25a1dfaf5c
Raza Ali <ra5458265@gmail.com>
2024-10-01 05:21:02 -0700

    all files

---
+++ Lesson1.js
@@ -0,0 +1,68 @@
+                        //console
+// console.log(45);
+// console.log(56);
+
+
+                        //comment
+// // Prints 5 to the console
+// console.log(5);
+// console.log(5);  // Prints 5 
+// console.log(/*IGNORED!*/ 5);  // Still just prints 5 
+
+                        // DataType
+// console.log('JavaScript');
+// console.log(2011);
+// console.log('Woohoo! I love to code! #codecademy');
+// console.log(20.49);
+
+
+                        //Arithemtic operator
+// console.log(10 + 3.5)
+// console.log(2022 - 1969);
+// console.log(65 / 240);
+// console.log(0.2708 * 100);
+
+// console.log(3+4)
+// // The first line of code is an arithmetic operator telling the computer to print the result of 3+4. The result will be 7
+// console.log('3+4');
+// // The Second is a string, telling the computer to print what is inside the parenthesis (as text). The result will be 3+4
+// console.log('3'+'4');
+// // The last is a combination of two strings, Telling the computer to display the string next to each other (combine) the texts. The result will be 34
+
+// 20 goes into -5 how many times? 0 right? What is the remainder? -5 correct?
+// -5 + 20 = 15, but -5 % 20 = -5
+// The output is correct.
+// > -5 + 20
+// 15
+// > -5 % 20
+// -5
+
+//String Concatenation
+// console.log('hi' + 'ya'); // Prints 'hiya'
+// console.log('wo' + 'ah'); // Prints 'woah'
+// console.log('I love to ' + 'code.')
+// // Prints 'I love to code.'
+
+// console.log('front ' + 'space'); 
+// // Prints 'front space'
+// console.log('back' + ' space'); 
+// // Prints 'back space'
+// console.log('no' + 'space'); 
+// // Prints 'nospace'
+// console.log('middle' + ' ' + 'space'); 
+// // Prints 'middle space'
+
+// console.log('One' + ', ' + 'two' + ', ' + 'three!'); 
+// // Prints 'One, two, three!
+
+// console.log('Hello' + 'World');
+// console.log('Hello'+ ' ' + 'World');
+
+// question:Can a space be at the beginning or end of a string?
+// answer:Yes! Any characters you put in a string will be displayed, and space is a character just like any other.
+
+// question:Why would we use string concatenation?
+// Answer:String concatenation refers to the process of combining two or more strings into a single string. It can be done by either appending one string to another or creating a new string that contains the original strings in sequence.
+
+// console.log('Teaching the world how to code'.length);
+console.log('Teaching the world how to code');
